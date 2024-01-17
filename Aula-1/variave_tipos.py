5eca057d6b231250f6fe060b373e7e17841ecad5
Jan Marchi <85848101+JanMarchi@users.noreply.github.com>
2024-01-17 07:49:16 -0300

    variavees

---
+++ Aula-1/variave_tipos.py
@@ -0,0 +1,43 @@
+
+#Inteiro (int):  Armazena números inteiros.
+
+idade = 23
+
+
+#  Ponto flutuante (float): Armazena números decimais.
+
+altura = 1.75
+
+
+# String (str):  Armazena sequências de caracteres.
+
+nome = "João"
+
+
+# Booleano (bool):  Armazena valores lógicos True ou False.
+
+tem_carteira_de_motorista = True
+
+
+# Lista (list): Armazena uma coleção ordenada de elementos.
+
+nomeros = [2, 3, 5, 6, 12, 14, 30]
+
+
+# tupla (tuple):  Semelhante a uma list, mas ela é imutavel.
+
+coordenadas = (10, 35)
+
+
+# Dicionario (dict):  Armazena pares chave-valot
+
+persona = {'nome': 'Maria', 'idade': 43 }
+
+
+# conjunto (set):  Armazena uma coleção não orenada de elemento único.
+
+numero_unicos = {1, 2, 3, 4, 5}
+
+
+
+ 
